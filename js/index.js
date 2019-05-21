8f16d24c712ac4c204ef55d09ab6276923b9c69b
Anna W <anski.anna@gmail.com>
2019-05-21 08:43:15 +0100

    initial commit on new branch

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-const siteContent = {
-    "nav": {
-        "nav-item-1": "Sign Up",
-        "nav-item-2": "Log In"
-    }
-}
+// const siteContent = {
+//     "nav": {
+//         "nav-item-1": "Sign Up",
+//         "nav-item-2": "Log In"
+//     }
+// }
+
+const workoutA = document.querySelector(".one");
