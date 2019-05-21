399aa00a700fe81974619879d404433272c6eca3
Anna W <anski.anna@gmail.com>
2019-05-21 09:45:57 +0100

    added a form to type in the exersice, amount lifted, how many reps and the body target for workout A

--- js/index.js
+++ js/index.js
@@ -1,46 +1,44 @@
-const siteContent = {
-    "main-content": {
-        "workoutA-h4": "Workout A",
-        "exercise-content": "Exercise",
-        "amount-content": "Amount",
-        "reps-content": "Reps",
-        "bodytarget-content": "Body Target",
-        "workoutB-h4": "Workout B",
-        "exercise-content": "Exercise",
-        "amount-content": "Amount",
-        "reps-content": "Reps",
-        "bodytarget-content": "Body Target",
-        "workoutC-h4": "Workout C",
-        "exercise-content": "Exercise",
-        "amount-content": "Amount",
-        "reps-content": "Reps",
-        "bodytarget-content": "Body Target",
-        "workoutD-h4": "Workout D",
-        "exercise-content": "Exercise",
-        "amount-content": "Amount",
-        "reps-content": "Reps",
-        "bodytarget-content": "Body Target",
-    }
-}
+// const siteContent = {
+//     "main-content": {
+//         "workoutA-h4": "Workout A",
+//         "exercise-content": "Exercise",
+//         "amount-content": "Amount",
+//         "reps-content": "Reps",
+//         "bodytarget-content": "Body Target",
+//         "workoutB-h4": "Workout B",
+//         "exercise-content": "Exercise",
+//         "amount-content": "Amount",
+//         "reps-content": "Reps",
+//         "bodytarget-content": "Body Target",
+//         "workoutC-h4": "Workout C",
+//         "exercise-content": "Exercise",
+//         "amount-content": "Amount",
+//         "reps-content": "Reps",
+//         "bodytarget-content": "Body Target",
+//         "workoutD-h4": "Workout D",
+//         "exercise-content": "Exercise",
+//         "amount-content": "Amount",
+//         "reps-content": "Reps",
+//         "bodytarget-content": "Body Target",
+//     }
+// }
 
-// MIDDLE CONTENT Workouts
+// // MIDDLE CONTENT Workouts
 
-let workoutA = document.querySelector('body > div > section > div:nth-child(1) > h4');
-workoutA.textContent = siteContent["main-content"]["workoutA-h4"];
+// let workoutA = document.querySelector('body > div > section > div:nth-child(1) > h4');
+// workoutA.textContent = siteContent["main-content"]["workoutA-h4"];
 
-let excercise = document.querySelector('body > div > section > div:nth-child(1) > p:nth-child(2)');
-excercise.textContent = siteContent["main-content"]["exercise-content"];
+// let excercise = document.querySelector('body > div > section > div:nth-child(1) > p:nth-child(2)');
+// excercise.textContent = siteContent["main-content"]["exercise-content"];
 
-let amount = document.querySelector('body > div > section > div:nth-child(1) > p:nth-child(3)');
-amount.textContent = siteContent["main-content"]["amount-content"];
+// let amount = document.querySelector('body > div > section > div:nth-child(1) > p:nth-child(3)');
+// amount.textContent = siteContent["main-content"]["amount-content"];
 
-let reps = document.querySelector('body > div > section > div:nth-child(1) > p:nth-child(4)');
-reps.textContent = siteContent["main-content"]["reps-content"];
+// let reps = document.querySelector('body > div > section > div:nth-child(1) > p:nth-child(4)');
+// reps.textContent = siteContent["main-content"]["reps-content"];
 
-let bodyTarget = document.querySelector('body > div > section > div:nth-child(1) > p:nth-child(3)');
-bodyTarget.textContent = siteContent["main-content"]["bodytarget-content"];
-
-
+// let bodyTarget = document.querySelector('body > div > section > div:nth-child(1) > p:nth-child(3)');
+// bodyTarget.textContent = siteContent["main-content"]["bodytarget-content"];
 
 
 
@@ -49,30 +47,32 @@ bodyTarget.textContent = siteContent["main-content"]["bodytarget-content"];
 
 
 
-class Article {
-    constructor(domElement) {
-      // assign this.domElement to the passed in domElement
-      this.domElement = domElement;
+
+
+// class Article {
+//     constructor(domElement) {
+//       // assign this.domElement to the passed in domElement
+//       this.domElement = domElement;
   
-      // create a reference to the ".expandButton" class. 
-      const expandButton = document.querySelectorAll('.expandButton');
+//       // create a reference to the ".expandButton" class. 
+//       const expandButton = document.querySelectorAll('.expandButton');
      
-      // Using your expandButton reference, update the text on your expandButton to say "expand"
-      expandButton.textContent = "expand";
+//       // Using your expandButton reference, update the text on your expandButton to say "expand"
+//       expandButton.textContent = "expand";
   
-      // Set a click handler on the expandButton reference, calling the expandArticle method.
-    //   expandButton.addEventListener('click', this.expandArticle.bind(this));
-    }
+//       // Set a click handler on the expandButton reference, calling the expandArticle method.
+//     //   expandButton.addEventListener('click', this.expandArticle.bind(this));
+//     }
   
-    expandArticle(event) {
-      // Using our reference to the domElement, toggle a class to expand or hide the article.
-      this.domElement.classList.toggle('article-open');
-    }  
-  }
+//     expandArticle(event) {
+//       // Using our reference to the domElement, toggle a class to expand or hide the article.
+//       this.domElement.classList.toggle('article-open');
+//     }  
+//   }
 
 
-const articles = document.querySelectorAll('.articles');
+// const articles = document.querySelectorAll('.articles');
 
 
-articles.forEach(article => new Article(article));
+// articles.forEach(article => new Article(article));
 
