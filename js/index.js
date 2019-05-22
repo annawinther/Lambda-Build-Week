3e3e911937a174f9e52f2e8a4650740026143108
Anna W <anski.anna@gmail.com>
2019-05-22 13:34:09 +0100

    menu sidebar dropdown added

--- js/index.js
+++ js/index.js
@@ -13,4 +13,17 @@
 
 // const deadlift = new Exercise("stronglifts","deadlift", "20kg", 5, "lower back");
 
-const workouts = [{}]
+const dropdown = document.querySelector(".dropdown-btn");
+
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
