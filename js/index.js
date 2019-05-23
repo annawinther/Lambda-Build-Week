003ad37b0cf8b6b568f13a580fbea91c8b1a0d9f
Anna W <anski.anna@gmail.com>
2019-05-23 10:55:31 +0100

    changed menu so that for desktop the nev itemsm are showing, mobile the menu button comes in with toggle

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 // MENU SHOWING WHEN CLICKED
 const toggleMenu = () => {
- // Toggle the "menu--open" class on your menu refence. 
    menu.classList.toggle('dropdown-content');
  }
 
@@ -9,20 +8,23 @@ const menu = document.querySelector('.dropdown-content');
 
 const button = document.querySelector(".dropdown");
 
-button.addEventListener('click', toggleMenu )
+button.addEventListener('click', toggleMenu)
+// button.addEventListener('click', move)
+
 
 
 // HOVER OVER LEARN MORE 
-const buttons = document.querySelectorAll('.btn');
+const buttons = document.querySelectorAll('.btn1');
 
 buttons.forEach(button => button.addEventListener('mouseover', color ));
 buttons.forEach(button => button.addEventListener('mouseout', white));
 
 function color (event){
-  event.target.style.color = " yellow";
-  event.target.style.borderColor = "yellow";
+  event.target.style.color =  "#fcc419";
+  event.target.style.borderColor = " #fcc419";
 }
 
 function white (event){
-  event.target.style = "color: white";
+  event.target.style.color = "white";
+  event.target.style.borderColor = "white";
 }
